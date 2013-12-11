d2bfb863ccde6218f6f1c37a12ff78edf84484dc
Ben Collier <me@bencollier.net>
2013-12-11 23:28:19 +0000

    colour tweak - needs new launch screenshots

--- V-Cards/VCardsAppDelegate.h
+++ V-Cards/VCardsAppDelegate.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#define GREEN_COLOR [UIColor colorWithRed:32.0f/255.0f green:196.0f/255.0f blue:0 alpha:1.0]
+#define GREEN_COLOR [UIColor colorWithRed:32.0f/255.0f green:196.0f/255.0f blue:0 alpha:0.8]
 
 #ifndef DEBUG
 #define GOOGLE_TRACKING_ID @"UA-44442599-1"
